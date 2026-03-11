7a8f72d1b26bbc46213d7851aa0e1ebc2bf2f15d
Mariyya-glitch <4601414@spsd.sk.ca>
2026-03-11 12:08:37 -0600

    3/11/2026
    Finished product

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -3,16 +3,26 @@
 // 2/11/2026
 //
 
+// Global variables
+
 let centerX;
 let centerY;
 
 let currentBack = 0
+
+let tSize = 50
+
+// Sets things up
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   centerX = width/2
   centerY = height/2
 }
+
+// Draws all the important things such as
+// Background (and changes its colour)
+// The objects in the scene
 
 function draw() {
 
@@ -26,10 +36,13 @@ function draw() {
     background('#2A4252')
   }
   noStroke();
+  mar();
   rect(1, 750, 5000, 500)
   igloo();
-  cloud();
+  cloud(); // The protagonist
 }
+
+// Changes the background colour variable when the mouse is clicked
 
 function mousePressed() {
   if(mouseButton === CENTER){}
@@ -44,14 +57,32 @@ function mousePressed() {
     }
 }
 
+// Shape and position of igloo
+
 function igloo(){
   circle(centerX, centerY+200, 350)
   square(centerX+1, centerY+150, 230)
 }
+
+// Shape, position, and mouse controls of the cloud
 
 function cloud(){
   circle(mouseX, centerY - 200, 100)
   circle(mouseX + 50, centerY + 10 - 200, 70)
   circle(mouseX - 50, centerY + 10 - 200, 70)
 }
+
+// Colour and position of my name
+// If a key is pressed, it grows
+
+function mar(){
+  fill('#052159')
+  textSize(tSize)
+  text('Mariyya', centerX - 50, centerY - 300)
+  fill(255)
+  if(keyIsPressed === true) {
+    tSize  += 10
+  }
+}
+        
 

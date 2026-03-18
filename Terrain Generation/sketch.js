0ff66a1c5b807758bd59d2248e0dea38ae0c485a
Mariyya-glitch <4601414@spsd.sk.ca>
2026-03-18 12:15:50 -0600

    3/18/2026
    i cant understand

---
+++ Terrain Generation/sketch.js
@@ -0,0 +1,17 @@
+// Sketch Generation
+// Mariyya
+// 3/11/2026
+//
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function generateTerrrain(){
+  
+}

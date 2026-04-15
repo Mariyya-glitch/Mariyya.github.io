ba9d224939f38f6ef5c416a26b6cf10ed7cdcb1b
Mariyya-glitch <4601414@spsd.sk.ca>
2026-04-15 11:37:38 -0600

    4/15/2026
    It's finally done

--- Terrain Generation/sketch.js
+++ Terrain Generation/sketch.js
@@ -24,28 +24,32 @@ function generateTerrain(){
   //random height, to be 2D terrain
   let highest = 0;
   let highestX = 0;
-  let average = 
+  let numRect = 0;
+  let totalHeight = 0;
+  let average;
   for(let x = 0; x < width; x+=rectWidth){
     //generate random() (negative) height
     //eventually replace this with using noise()
     // let rectHeight = random(0, height*0.75);
     let rectHeight = noise(noiseTime);
+    numRect ++;
     rectHeight = map(rectHeight, 0, 1, 0, height*0.85);
     if (rectHeight > highest){
       highest = rectHeight;
       highestX = x;
     }
     rect(x,height,rectWidth,-rectHeight);
-    
+    totalHeight = totalHeight + rectHeight;
+    average = totalHeight / numRect;
     noiseTime += noiseSpeed;
   }
   drawFlag(highestX, height - highest);
   fill(0);
-  line(0, height - average, width);
+  line(0, height - average, width, height - average);
 }
 
 function keyPressed(){
- 
+ // right makes it wider and left makes it thinner
   if (keyCode === LEFT_ARROW){
     if(rectWidth > 1){
       rectWidth -= 1;
@@ -58,6 +62,7 @@ function keyPressed(){
 }
 
 function drawFlag(x,y){
+  //how to draw flag
   fill(0);
   rect(x, y, 3, -50);
   triangle(x + 3, y - 50, x + 3, y - 30, x + 20, y - 35)
